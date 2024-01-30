e4dd7cd986fba039507dd1c27f67302a4986c1bb
Evgeni Glushko <sstawr@gmail.com>
2024-01-30 20:31:02 +0300

    Initial Commit

---
+++ JokeApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  JokeApp
+//
+//  Created by Evgeni Glushko on 30.01.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
